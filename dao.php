132e3e338fd3499bdd1970309d195ea8c0a5e24d
Jesse Doty <jessedoty@u.boisestate.edu>
2016-11-13 10:51:41 -0700

    Added new user function

--- dao.php
+++ dao.php
@@ -2,20 +2,34 @@
 
 class Dao {
 
-    //Since repo is public, I changed this to dummy credentials
-    private $host = "localhost";
-    private $db = "database";
-    private $user = "user";
-    private $pass = "password";
+  private $host = "us-cdbr-iron-east-03.cleardb.net";
+  private $db = "heroku_4d19e91f7d43b1a";
+  private $user = "b3a51b586d0bd2";
+  private $pass = "a7e9651e";
 
-    public function getConnection () {
-     return
-          new PDO("mysql:host={$this->host};dbname={$this->db}", $this->user,
+  public function getConnection () {
+    return
+      new PDO("mysql:host={$this->host};dbname={$this->db}", $this->user,
           $this->pass);
-    }
+  }
 
-    public function new_user($email, $password) {
-      //sanitize data and add user to the db
-    }
+
+  /*
+   * Add  new user to database
+   */
+  public function new_user($email, $password) {
+      $cxn = $this->getConnection();
+      $newUserQuery = "INSERT INTO person 
+                          (email, password, prof_pic)
+                          VALUES 
+                          (:email, :password, LOAD_FILE(:prof_pic))";
+      $q = $cxn->prepare($newUserQuery);
+      $q->bindParam(":email", $email);
+      $q->bindParam(":password", $password);
+      $imgPath = "images/avatar.png";
+      $q->bindParam(":prof_pic", $imgPath);
+
+      $q->execute();
+  }
 
 }
