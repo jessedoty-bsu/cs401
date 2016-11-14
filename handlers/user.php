a006d567d35a4260dc9bcede9cba7f43414323cc
Jesse Doty <jessedoty@u.boisestate.edu>
2016-11-14 11:16:08 -0700

    This one should work

--- handlers/user.php
+++ handlers/user.php
@@ -2,7 +2,7 @@
     if(session_status() === PHP_SESSION_NONE) {
         session_start();
     }
-    require_once $_SERVER['DOCUMENT_ROOT'] . "../dao.php";
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/dao.php";
     $dao = new Dao();
 
     /*
