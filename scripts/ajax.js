4529e3a71e36379ea3e66207c8975af781cdb636
Jesse Doty <jessedoty@u.boisestate.edu>
2016-12-05 14:18:55 -0700

    Fixed bug where hitting cancel while adding a new item would still add it

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -25,53 +25,62 @@ function refreshListTabs() {
 /* Add a new list*/
 function newList() {
    var content = prompt("Please enter the item's name.", "New List");
-   content = content ? content:"New List";
-   console.log("Adding new list: " + content);
+   //Hitting cancel on the prompt returns null
+   if (content != null) {
+      //Check if user gave an empty string
+      content = content ? content : "New Item";
+      console.log("Adding new list: " + content);
 
-   var value = { "new_list_title":content };
-   $.ajax({
-      type: "POST",
-      url: "../handlers/list.php",
-      data: value,
-      success: function () {
-         //Prepend button for new list to list_ul
-         // $("#list_ul").prepend(
-         //     "<li><button id=\"" + content + "\">" + content + "</button></li>"
-         // );
+      var value = {"new_list_title": content};
+      $.ajax({
+         type: "POST",
+         url: "../handlers/list.php",
+         data: value,
+         success: function () {
+            //Prepend button for new list to list_ul
+            // $("#list_ul").prepend(
+            //     "<li><button id=\"" + content + "\">" + content + "</button></li>"
+            // );
 
-         //Refresh list view
-         refreshList();
+            //Refresh list view
+            refreshList();
 
-         //Reload share options since they have become enabled
-         $("#share_options").load(location.href+" #share_options>*","");
-      },
-      error: function () {
-         alert("New list failed.");
-      }
-   });
+            //Reload share options since they have become enabled
+            $("#share_options").load(location.href + " #share_options>*", "");
+         },
+         error: function () {
+            alert("New list failed.");
+         }
+      });
+   }
 }
 
 /* Add a new item to a list*/
 function newItem(id) {
    var content = prompt("Please enter the item's name.", "New Item");
-   content = content ? content:"New Item";
-   console.log("Adding new item.");
+   //Hitting cancel on the prompt returns null
+   if (content != null) {
+      //Check if user gave an empty string
+      content = content ? content : "New Item";
+      console.log("Adding new item.");
 
-   var values = { "list_id":id, "new_item_content":content };
-   $.ajax({
-      type: "POST",
-      url: "../handlers/list.php",
-      data: values,
-      success: function () {
-         //Refresh list view
-         refreshItems();
-      },
-      error: function () {
-         alert("New item failed.");
-      }
-   });
+      var values = {"list_id": id, "new_item_content": content};
+      $.ajax({
+         type: "POST",
+         url: "../handlers/list.php",
+         data: values,
+         success: function () {
+            //Refresh list view
+            refreshItems();
+         },
+         error: function () {
+            alert("New item failed.");
+         }
+      });
+   }
 }
 
+/* Toggle checked status of an item */
 function toggleChecked(id, checked) {
    console.log(id + " " + checked);
    var values = { "item_id":id, "checked":checked };
@@ -88,6 +97,7 @@ function toggleChecked(id, checked) {
    });
 }
 
+/* Switch the view to a different list */
 function switchToList(id) {
    var values = { "list_id":id };
    $.ajax({
@@ -103,21 +113,27 @@ function switchToList(id) {
    });
 }
 
+/* Share the given list with a user */
 function share(listID) {
    var user = prompt("Enter the email address of the user you would like to share this list with.");
-   if(!user) {
-      alert("Error: No email address was entered.");
-      return false;
-   }
+   //Hitting cancel on the prompt returns null
+   if (user != null) {
+       //If no email was given alert the user of invalid data and exit the call
+       if (!user) {
+           alert("Error: No email address was entered.");
+           return false;
+       }
 
-   var values = { "user":user, "share_list_id":listID };
-   $.ajax({
-      type: "POST",
-      url: "../handlers/list.php",
-      data: values,
-      success: function () {},
-      error: function () {
-         alert("List view switch failed.");
-      }
-   });
+       var values = {"user": user, "share_list_id": listID};
+       $.ajax({
+           type: "POST",
+           url: "../handlers/list.php",
+           data: values,
+           success: function () {
+           },
+           error: function () {
+               alert("List view switch failed.");
+           }
+       });
+   }
 }
